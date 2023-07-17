41d9123e53ac0027ec79b886e06eaae6beaf74bc
Vertori <115083330+Vertori@users.noreply.github.com>
2023-07-17 11:21:55 +0200

    website scheme /w routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import AnimRoutes from "./components/AnimRoutes";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { motion } from "framer-motion";
 
 function App() {
-
   return (
     <>
-
+      <Router>
+        <Header />
+        <AnimRoutes />
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
