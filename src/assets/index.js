5c1cfc109f2000ca67e2b62964b9fcaef943be96
Vertori <115083330+Vertori@users.noreply.github.com>
2023-07-18 11:19:52 +0200

    home page

---
+++ src/assets/index.js
@@ -0,0 +1,151 @@
+import htmlLogo from "./techLogos/htmlLogo.svg";
+import cssLogo from "./techLogos/cssLogo.svg";
+import jsLogo from "./techLogos/jsLogo.svg";
+import reactLogo from "./techLogos/reactLogo.svg";
+import tailwindLogo from "./techLogos/tailwindLogo.svg";
+import firebaseLogo from "./techLogos/firebaseLogo.svg";
+import styledcomponentsLogo from "./techLogos/styledcomponentsLogo.svg";
+import framermotionLogo from "./techLogos/framermotionLogo.svg";
+import githubLogo from "./techLogos/githubLogo.svg";
+import gitLogo from "./techLogos/gitLogo.svg";
+import typescriptLogo from "./techLogos/typescriptLogo.svg";
+import reduxLogo from "./techLogos/reduxLogo.svg";
+import netflix1 from "./websites/netflix1.webp";
+import netflix2 from "./websites/netflix2.webp";
+import netflix3 from "./websites/netflix3.webp";
+import ecommerce1 from "./websites/ecommerce1.webp";
+import ecommerce2 from "./websites/ecommerce2.webp";
+import ecommerce3 from "./websites/ecommerce3.webp";
+import yt1 from "./websites/yt1.webp";
+import yt3 from "./websites/yt3.webp";
+import tesla1 from "./websites/tesla1.webp";
+import tesla2 from "./websites/tesla2.webp";
+
+const techList = [
+  {
+    id: 1,
+    name: "HTML",
+    image: htmlLogo,
+  },
+  {
+    id: 2,
+    name: "CSS",
+    image: cssLogo,
+  },
+  {
+    id: 3,
+    name: "JavaScript",
+    image: jsLogo,
+  },
+  {
+    id: 4,
+    name: "React JS",
+    image: reactLogo,
+  },
+  {
+    id: 5,
+    name: "Tailwind CSS",
+    image: tailwindLogo,
+  },
+  {
+    id: 6,
+    name: "Styled Components",
+    image: styledcomponentsLogo,
+  },
+  {
+    id: 7,
+    name: "Firebase",
+    image: firebaseLogo,
+  },
+  {
+    id: 8,
+    name: "Framer Motion",
+    image: framermotionLogo,
+  },
+  {
+    id: 9,
+    name: "Git",
+    image: gitLogo,
+  },
+  {
+    id: 10,
+    name: "GitHub",
+    image: githubLogo,
+  },
+];
+
+const currentlyLearning = [
+  {
+    id: 1,
+    name: "Typescript",
+    image: typescriptLogo,
+  },
+  {
+    id: 2,
+    name: "Redux",
+    image: reduxLogo,
+  },
+];
+
+const projectsList = [
+  {
+    id: 1,
+    name: "Netflix Clone",
+    description:
+      "Netlix Clone created by fetching data from TMDb API, fully styled using Tailwind CSS. Thanks to Firebase, the website allows signing in, signing up, saving movies for signed in users and deleting them from saved list. Account page is only available to signed in users, which is provided by protected route. ",
+    images: [netflix1, netflix2, netflix3],
+    tags: [
+      "React",
+      "Tailwind CSS",
+      "API (Axios)",
+      "Firebase",
+      "React Router",
+      "Swiper",
+    ],
+    liveLink: "https://netflix-clone-react-rf.netlify.app/",
+    codeLink: "https://github.com/Vertori/ntflx-cln-react",
+  },
+  {
+    id: 2,
+    name: "E-commerce website",
+    description:
+      "Online store website created by fetching data from Fake Store API, fully styled using Tailwind CSS. The website allows you to add items to the cart and fully manipulate cart content. The shopping cart dynamically updates the number of items in it and their total value. Each product has its own dynamically generated page.",
+    images: [ecommerce1, ecommerce2, ecommerce3],
+    tags: ["React", "Tailwind CSS", "API (Axios)", "React Router", "Swiper"],
+    liveLink: "https://ecmrc-react-rf.netlify.app/",
+    codeLink: "https://github.com/Vertori/e-cmrc-react",
+  },
+  {
+    id: 3,
+    name: "YouTube Clone",
+    description:
+      "Website created by fetching data from API, fully styled using Tailwind CSS. It allows you to search for movies and channels, to watch movies and explore channels content. Each movie includes its own dynamically generated page that contains information about the movie and displays other related movies.",
+    images: [yt1, yt3],
+    tags: [
+      "React",
+      "Tailwind CSS",
+      "API (Axios)",
+      "React Router",
+      "React Player",
+    ],
+    liveLink: "https://yt-clone-react-rf.netlify.app/",
+    codeLink: "https://github.com/Vertori/yt-cln-react",
+  },
+  {
+    id: 4,
+    name: "Tesla Clone",
+    description:
+      "Tesla Clone website created using Styled Components. This is a simple page that, using a single component that accepts props, generates sections for each brand model. Sections are animated using React Awesome Reveal, and scrolling the mouse causes the website to jump to the next sections.",
+    images: [tesla1, tesla2],
+    tags: [
+      "React",
+      "Styled Components",
+      "React Router",
+      "React Awesome Reveal",
+    ],
+    liveLink: "https://tesla-clone-react-rf.netlify.app/",
+    codeLink: "https://github.com/Vertori/tsl-cln-react",
+  },
+];
+
+export { techList, currentlyLearning, projectsList };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import avatar from "../assets/avatar1.webp";
+import Socials from "../components/Socials";
+import { motion } from "framer-motion";
+import { transition1 } from "../transitions";
 
-const Home = () => {
+const Home = (): JSX.Element => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="section flex items-center justify-center px-4">
+      <div className="container h-full flex flex-col lg:flex-row items-center gap-x-32 pt-36 xl:pt-0">
+        <Socials />
+        {/* text */}
+        <div className="text-center lg:text-start">
+          <h1 className="h1">Rafał Fikus </h1>
+          <p className="text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12">
+            Frontend React Developer
+          </p>
+          <Link to={"/projects"} className="btn mb-[30px]">
+            Check my projects
+          </Link>
+        </div>
+        {/* img */}
+        <div className="max-w-[200px] md:max-w-full">
+          <img className="rounded-full shadow-2xl" src={avatar} alt="avatar" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
